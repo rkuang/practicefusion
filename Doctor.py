ee9a78bf6e8c0fcb08f3a42936f7eb2178e76886
Ricky Kuang <rkuang26@gmail.com>
2018-06-30 14:19:24 -0700

    Doctor class and constructor

---
+++ Doctor.py
@@ -0,0 +1,7 @@
+class Doctor:
+    def __init__(self, first="", last="", spec="", area="", score=0.0):
+        self.first_name = first
+        self.last_name = last
+        self.specialty = spec
+        self.area = area
+        self.review_score = score
