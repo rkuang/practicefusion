27340134b4c9a820ebf3823c7f968bb986794f0a
Ricky Kuang <rkuang26@gmail.com>
2018-06-30 14:20:11 -0700

    use template in application

--- practicefusion.py
+++ practicefusion.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask import render_template
+from Doctor import Doctor
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "hello world"
+    doctor = Doctor(first="Ricky", last="Kuang", spec="Dermatology", area="Alhambra, CA", score=5.0)
+    return render_template('index.html', doc=doctor)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
